234e31adee2f9978f855d8d0b38c22d6fc69cfd8
Your Name <email@address.com>
2021-04-08 14:26:44 +0300

    5.2

--- index.php
+++ index.php
@@ -12,8 +12,14 @@ $c = readline();
 
 $sqr = new Square();
     MyLog::log("The equation is: ". $a.'x^2 + '.$b.'x + '.$c.' = 0'. PHP_EOL);
+    if ($a == 0) {
+        MyLog::log("It is a linear equation.");
+    } else {
+        MyLog::log("It is a quad equation.");
+    }
 
     $res = $sqr->solve($a, $b, $c);
+    MyLog::log("Equation roots: ");
     foreach ($res as $el) {
         MyLog::log($el);
     }
